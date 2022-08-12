b388204ced443887b0bfdf1bb0f411fa8c9a51da
Rick Hausz <pinkhendrix1881@gmail.com>
2022-08-12 14:21:07 -0700

    Added our layers and ran a Test Generate

--- Happy_Faces_ArtEngine/src/config.js
+++ Happy_Faces_ArtEngine/src/config.js
@@ -13,12 +13,10 @@ const layerConfigurations = [
     growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Eyes" },
+      { name: "Face" },
+      { name: "Hair" },
+      { name: "Mouth" },
     ],
   },
 ];
